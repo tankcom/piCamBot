6f3a64869a5d13da0024faf35b3d9f6274fa75f0
tankcom <mac>
2018-05-26 23:16:01 +0200

    bad practice

--- piCamBot.py
+++ piCamBot.py
@@ -188,7 +188,7 @@ class piCamBot:
             args = ['bash', '-c', "ffmpeg -f concat -safe 0 -r 20 -i <(ls -d -1 /tmp/piCamBot/video/data/*jpg | sed 's/^/file /') -vf format=yuv420p -c h264_omx /tmp/piCamBot/a2.mp4"]
             # args = ['echo' 'kek']
             try:
-                subprocess.Popen(args, stderr='/dev/null')
+                subprocess.Popen(args, stderr='/dev/null', stdout='/dev/null')
             except Exception:
                 print('kek')
                 pass
