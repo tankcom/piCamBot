80bd97616c1938445954d0930a168b82f49fe32d
tankcom <mac>
2018-05-28 23:26:39 +0200

    fixed typo

--- piCamBot.py
+++ piCamBot.py
@@ -805,12 +805,12 @@ class piCamBot:
                             pass
                 self.isPictureMoved = True
                 time.sleep(4)
-            try:
-                isNotEmpty2 = os.listdir('/tmp/piCamBot/video/tmp')
-            except Exception as e:
-                print(e)
-                pass
-            if self.isPictureMoved and isNotEmpty2 and not self.ffmpegrunning: #only execute if pictures have been moved and the input folder is not empty and ffmpeg is not currently running
+            #try:
+            #    isNotEmpty2 = os.listdir('/tmp/piCamBot/video/tmp')
+            #except Exception as e:
+            #    print(e)
+            #    pass
+            if self.isPictureMoved and not self.ffmpegrunning: #only execute if pictures have been moved and the input folder is not empty and ffmpeg is not currently running
                 self.ffmpegrunning = True # tell if ffmpeg is started
                 args = ['bash', '-c', "ffmpeg -f concat -safe 0 -r 7 -i <(ls -d -1 /tmp/piCamBot/video/tmp/*.jpg | sed 's/^/file /') -c h264_omx -b:v 800k /tmp/piCamBot/video/tmp4/a2.mp4; rm /tmp/piCamBot/video/tmp/*.jpg"]
                 try:
