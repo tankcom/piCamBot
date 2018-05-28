bb8c15757c30431fa21fed25f4f0b5bb9b0c4b35
tankcom <mac>
2018-05-28 22:42:17 +0200

    fixed typo

--- piCamBot.py
+++ piCamBot.py
@@ -814,7 +814,7 @@ class piCamBot:
                 pass
             if self.isPictureMoved and isNotEmpty2 and not self.ffmpegrunning: #only execute if pictures have been moved and the input folder is not empty and ffmpeg is not currently running
                 self.ffmpegrunning = True # tell if ffmpeg is started
-                args = ['bash', '-c', "ffmpeg -f concat -safe 0 -r 10 -i <(ls -d -1 /tmp/piCamBot/video/tmp/*.jpg | sed 's/^/file /') -vf format=yuv420p -c copy -b:v 800k /tmp/piCamBot/video/tmp4/a2.mp4; rm /tmp/piCamBot/video/tmp/*.jpg"]
+                args = ['bash', '-c', "ffmpeg -f concat -safe 0 -r 10 -i <(ls -d -1 /tmp/piCamBot/video/tmp/*.jpg | sed 's/^/file /') -c copy -b:v 800k /tmp/piCamBot/video/tmp4/a2.mp4; rm /tmp/piCamBot/video/tmp/*.jpg"]
                 try:
                     subprocess.Popen(args)
                     print('ffmpeg starting up')
